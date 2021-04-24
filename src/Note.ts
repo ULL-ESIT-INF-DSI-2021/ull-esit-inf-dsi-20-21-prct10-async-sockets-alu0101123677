ee4801815b40603ac2d84d36a76fffb07fd4a8ef
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-04-24 19:00:34 +0100

    ver 0.2

--- src/Note.ts
+++ src/Note.ts
@@ -1,7 +1,9 @@
+import * as chalk from 'chalk'
 type Color = 'Rojo' | 'Verde' | 'Azul' | 'Amarillo'
 
+
 export class Note {
-  constructor(protected title: string, protected body: string, protected color: string){}
+  constructor(protected title: string, protected body: string, protected color: Color){}
 
   getTitle(): string{
     return this.title;
@@ -23,7 +25,49 @@ export class Note {
     this.body = body;
   }
 
-  setColor(color: string) {
+  setColor(color: Color) {
     this.color = color
   }
+
+  printTitle(){
+    switch (this.color) {
+      case "Rojo":
+        console.log(chalk.red(this.title));
+        break;
+      case "Verde":
+        console.log(chalk.green(this.title));
+        break;
+      case "Azul":
+        console.log(chalk.blue(this.title));
+        break;
+      case "Amarillo":
+        console.log(chalk.yellow(this.title));
+        break;
+      default:
+        console.log(chalk.red("Color no valido para la nota"));
+        break;
+    }
+  }
+
+  printBody(){
+    switch (this.color) {
+      case "Rojo":
+        console.log(chalk.red(this.body));
+        break;
+      case "Verde":
+        console.log(chalk.green(this.body));
+        break;
+      case "Azul":
+        console.log(chalk.blue(this.body));
+        break;
+      case "Amarillo":
+        console.log(chalk.yellow(this.body));
+        break;
+      default:
+        console.log(chalk.red("Color no valido para la nota"));
+        break;
+    }
+  }
 }
+
+
--- src/note-app.ts
+++ src/note-app.ts
@@ -5,6 +5,11 @@ yargs.command({
   command: 'add',
   describe: 'Añade una nueva nota',
   builder: {
+    user: {
+      describe: 'Usuario de la nota',
+      demandOption: true,
+      type: 'string',
+    },
     title: {
       describe: 'Titulo de la nota',
       demandOption: true,
@@ -15,13 +20,101 @@ yargs.command({
       demandOption: true,
       type: 'string'
     },
-    Color: {
+    color: {
       describe: 'Color de la nota',
       demandOption: true,
       type: 'string'
     },
   },
   handler(argv) {
-
+    console.log('añadir')
   },
 });
+
+yargs.command({
+  command: 'modify',
+  describe: 'Modifica una nueva nota',
+  builder: {
+    user: {
+      describe: 'Usuario de la nota',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Titulo de la nota',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Cuerpo de la nota',
+      demandOption: true,
+      type: 'string'
+    },
+    color: {
+      describe: 'Color de la nota',
+      demandOption: true,
+      type: 'string'
+    },
+  },
+  handler(argv) {
+    console.log('añadir')
+  },
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'List las notas de un usuario',
+  builder: {
+    user: {
+      describe: 'Usuario de la notas',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    console.log('listado')
+  },
+});
+
+
+yargs.command({
+  command: 'read',
+  describe: 'Lee la nota de un usuario',
+  builder: {
+    user: {
+      describe: 'Usuario de la nota',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Titulo de la nota',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    console.log('leer')
+  },
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Eliminar la nota de un usuario',
+  builder: {
+    user: {
+      describe: 'Usuario de la nota',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Titulo de la nota',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    console.log('remover')
+  },
+});
+
+yargs.parse();
